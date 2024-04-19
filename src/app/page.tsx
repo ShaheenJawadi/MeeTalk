ad1800512d145b0cd8940f4776acd10714780b5e
shaheenj <jawadishaheen@gmail.com>
2024-04-19 05:19:23 +0100

    Add socket.io-client dependency and initialize a simple socket connection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import BottomBar from "@/components/conversation/bottomBar";
 import MessagesContainer from "@/components/conversation/messages/container";
+import * as  io from "socket.io-client";
 
 export default function Home() {
+  const socket = io.connect("http://localhost:3500");
   return (
     <main className={styles.main}>
       <MessagesContainer/>
