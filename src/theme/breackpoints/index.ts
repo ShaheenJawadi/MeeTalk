53c17528cf150da93b1dd759c330cc15d88fbcfe
shaheenj <jawadishaheen@gmail.com>
2024-04-20 00:00:24 +0100

    Add breakpoints theme file

---
+++ src/theme/breackpoints/index.ts
@@ -0,0 +1,13 @@
+ 
+ const breakpoints =()=>({
+    values: {
+        xs: 0,
+        sm: 600,
+        md: 960,
+        lg: 1200,
+        xl: 1500,
+    },
+ })
+ 
+
+export default breakpoints;
