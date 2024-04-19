107f9f4b0a29e5232f354c9079810bbbb20f0869
shaheenj <jawadishaheen@gmail.com>
2024-04-19 05:46:45 +0100

    Refactor page.tsx to add message input and send functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,30 @@
-import Image from "next/image";
+"use client";
 import styles from "./page.module.css";
 import BottomBar from "@/components/conversation/bottomBar";
 import MessagesContainer from "@/components/conversation/messages/container";
 import * as  io from "socket.io-client";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const socket = io.connect("http://localhost:3500");
+
+  const [message, setMessage] = useState(""); 
+  function sendMessage() { 
+    socket.emit("send_msg", { message: message });
+  }
+
+
   return (
     <main className={styles.main}>
-      <MessagesContainer/>
-      <BottomBar/>
+
+      <div>
+
+        <input type="text" onChange={(e) => {
+          setMessage(e.target.value);
+        }} placeholder="Type a message" />
+
+        <button onClick={sendMessage} >Send</button>
+      </div>
     </main>
   );
 }
