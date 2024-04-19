a610449527e5c30a0799c3fdead775efce93ab84
shaheenj <jawadishaheen@gmail.com>
2024-04-19 23:11:15 +0100

    Refactor layout.tsx to add MUI theme provider and AppRouterCacheProvider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,30 +4,30 @@ import BottomBar from "@/components/conversation/bottomBar";
 import MessagesContainer from "@/components/conversation/messages/container";
 import * as  io from "socket.io-client";
 import { useEffect, useState } from "react";
-
-export default function Home() {
   const socket = io.connect("http://localhost:3500");
+export default function Home() {
 
-  const [message, setMessage] = useState(""); 
-  const [recivedMessages, setRecivedMessages] = useState<string[]>([]);
-  function sendMessage() { 
-    socket.emit("send_msg", { message: message });
-  }
 
-  useEffect(() => {
-    socket.on("receive_msg", (data) => {  
-      setRecivedMessages(recivedMessages.concat(data.message));
-    });
-  }, [socket]);
+    const [message, setMessage] = useState(""); 
+    const [recivedMessages, setRecivedMessages] = useState<string[]>([]);
+    function sendMessage() { 
+      socket.emit("send_msg", { message: message });
+    }
+
+    useEffect(() => {
+      socket.on("receive_msg", (data) => { 
+        setRecivedMessages(recivedMessages.concat(data.message));
+      });
+    }, [socket]);
 
   return (
     <main className={styles.main}>
 
-    {
-      recivedMessages.map((msg, index) => {
-        return <div key={index}>{msg}</div>;
-      })
-    }
+      {
+        recivedMessages.map((msg, index) => {
+          return <div key={index}>{msg}</div>;
+        })
+      }
       <div>
 
         <input type="text" onChange={(e) => {
