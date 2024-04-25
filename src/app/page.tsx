d6f41abb37878e50624b3d9e0d8fc77716cc28c6
shaheenj <jawadishaheen@gmail.com>
2024-04-25 04:35:46 +0100

    Add Tab component and implement chat tabs in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,11 @@
 import styles from "./page.module.css";
 import BottomBar from "@/components/conversation/bottomBar";
 import * as  io from "socket.io-client";
-import { useEffect, useState } from "react";
+import { useEffect, useState } from "react"; 
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
 import { Avatar, Box, Button, Container, Grid, Paper, Stack, Typography } from "@mui/material";
 const socket = io.connect("http://localhost:3500");
 export default function Home() {
@@ -60,6 +64,21 @@ export default function Home() {
 
             </Stack>
             <Typography variant="h4">Chats</Typography>
+            <TabContext value={"1"}>
+              <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                <TabList    >
+                  <Tab label="M" value="1" />
+                  <Tab label="F" value="2" />
+                  <Tab label="C" value="3" />
+                  <Tab label="C" value="4" />
+                </TabList>
+              </Box>
+              <TabPanel value="1">Messages</TabPanel>
+              <TabPanel value="2">Fav</TabPanel>
+              <TabPanel value="3">calls</TabPanel>
+              <TabPanel value="4">contacts</TabPanel>
+            </TabContext>
+            
           </Stack>
         </Grid>
         <Grid item md={9}>
