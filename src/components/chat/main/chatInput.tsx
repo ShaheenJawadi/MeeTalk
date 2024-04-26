7538877a00aa411a22df53249d7c688c988e5990
shaheenj <jawadishaheen@gmail.com>
2024-04-26 05:29:40 +0100

    Update chat input component

--- src/components/chat/main/chatInput.tsx
+++ src/components/chat/main/chatInput.tsx
@@ -1,24 +1,41 @@
-import { Stack ,Box, TextField, Button } from '@mui/material';
+
+import { Stack, Box, IconButton, OutlinedInput, InputAdornment } from '@mui/material';
+import { FileImage, Send, Paperclip, EmoticonHappyOutline, Microphone } from 'mdi-material-ui';
 import React from 'react';
 
 const ChatInput = () => {
 
     return (
-        <Stack direction={"row"} spacing={4} alignItems={"center"} padding={3} >
-            <Box>Emoji</Box>
+        <Stack direction={"row"} spacing={4} alignItems={"end"} padding={3} >
+           
+            <Box><FileImage sx={{ fontSize: 22, color: "primary.main" }} /></Box>
+            <Box><Paperclip sx={{ fontSize: 22, color: "primary.main" }} /></Box>
+            <Box><Microphone sx={{ fontSize: 22, color: "primary.main" }} /></Box>
             <Box flex={1} >
-                            <TextField fullWidth placeholder='Message' id="fullWidth" />
+
+                <OutlinedInput fullWidth placeholder='Message'
+                        size="small"
+                        multiline
+                        maxRows={3}
+                    endAdornment={
+                        <InputAdornment position="end">
+                            <IconButton    >
+                                <EmoticonHappyOutline sx={{ fontSize: 24, color: "primary.main" }} />
+                            </IconButton >
+                        </InputAdornment>
+                    } 
+                />
+                
 
             </Box>
-            <Box>IMG</Box>
-            <Box>
-               
-                <Box>
-                    <Button variant="contained" color="primary">
-                        send
-                    </Button>
-                </Box>
+           
+
+            <Box >
+                <IconButton    >
+                    <Send sx={{ fontSize: 25 , color:"primary.main" }} />
+                </IconButton >
             </Box>
+
         </Stack>
     );
 };
