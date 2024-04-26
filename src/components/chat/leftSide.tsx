c86c9ab84178f0d626c5a2c6827f4784b4a64f01
shaheenj <jawadishaheen@gmail.com>
2024-04-26 07:13:51 +0100

    Refactor LeftSide component

--- src/components/chat/leftSide.tsx
+++ src/components/chat/leftSide.tsx
@@ -1,42 +1,44 @@
 import React from 'react';
- 
+
 import Tab from '@mui/material/Tab';
 import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
-import { Avatar, Box,   Stack, Typography } from "@mui/material";
- 
+import { Avatar, Box, Divider, Stack, Typography } from "@mui/material";
+import { Message, StarFourPoints, AccountBox, Phone, CogOutline } from 'mdi-material-ui';
 
 const LeftSide = () => {
-  
+
     return (
-        <Stack padding={5}  >
-            <Stack direction={"row"}   >
+        <Stack   >
+            <Stack padding={5}  direction={"row"} justifyContent={"space-between"} alignItems={"center"} >
+                <Stack direction={"row"} spacing={2} alignItems={"center"}>
 
-                <Avatar alt="Shaheen jawadi" />
 
-                <Stack  >
-                    <Typography variant="h6">Shaheen Jawadi</Typography>
-                    <Typography variant="caption">@Shaheenj</Typography>
+                    <Avatar alt="Shaheen jawadi" variant='rounded' />
+
+                    <Stack>
+                        <Typography variant="h6">Shaheen Jawadi</Typography>
+                        <Typography variant="caption">@Shaheenj</Typography>
+                    </Stack>
+
                 </Stack>
+                <CogOutline sx={{ fontSize:14 }} />
 
-
-
-            </Stack>
-            <Typography variant="h4">Chats</Typography>
+            </Stack> 
             <TabContext value={"1"}>
                 <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                    <TabList    >
-                        <Tab label="M" value="1" />
-                        <Tab label="F" value="2" />
-                        <Tab label="C" value="3" />
-                        <Tab label="C" value="4" />
+                    <TabList centered    >
+                        <Tab sx={{ p: 0 }} icon={<Message />} value="1" />
+                        <Tab sx={{ p: 0 }} icon={<StarFourPoints />} value="2" />
+                        <Tab sx={{ p: 0 }} icon={<AccountBox />} value="3" />
+                        <Tab sx={{ p: 0 }} icon={<Phone />} value="4" />
                     </TabList>
                 </Box>
                 <TabPanel value="1">Messages</TabPanel>
                 <TabPanel value="2">Fav</TabPanel>
-                <TabPanel value="3">calls</TabPanel>
-                <TabPanel value="4">contacts</TabPanel>
+                <TabPanel value="3">contacts</TabPanel>
+                <TabPanel value="4"> calls</TabPanel>
             </TabContext>
 
 
