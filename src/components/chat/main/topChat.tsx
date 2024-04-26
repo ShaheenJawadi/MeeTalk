58f9fdc652fc87e7969ebddcd5055603e6674a4c
shaheenj <jawadishaheen@gmail.com>
2024-04-26 02:52:59 +0100

    Update TopChat Structure

--- src/components/chat/main/topChat.tsx
+++ src/components/chat/main/topChat.tsx
@@ -1,11 +1,27 @@
-import { Stack } from '@mui/material';
+import { Stack, Box, Avatar, Typography } from '@mui/material';
 import React from 'react';
 
 const TopChat = () => {
 
     return (
-        <Stack>
-            top
+        <Stack padding={2} direction={"row"} alignItems={"center"} justifyContent={"space-between"}>
+            <Box>
+                <Stack direction={"row"} alignItems={"center"} spacing={2}  >
+
+                    <Avatar alt="Shaheen jawadi" />
+
+                    <Stack  >
+                        <Typography variant="body1">Shaheen Jawadi</Typography>
+                        <Typography variant="subtitle2">@Shaheenj</Typography>
+                    </Stack>
+
+
+
+                </Stack>
+            </Box>
+            <Box>
+                Call
+            </Box>
         </Stack>
     );
 };
