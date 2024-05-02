77e8438508c8901919ee29c9a67fa563d98fb8bf
shaheenj <jawadishaheen@gmail.com>
2024-05-02 22:19:27 +0100

    Remove commented code in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,40 +13,7 @@ export default function Home() {
  
   const dispatch = useDispatch<AppDispatch>()
   dispatch(initIO());
-
-  //   const [message, setMessage] = useState(""); 
-  //   const [recivedMessages, setRecivedMessages] = useState<string[]>([]);
-  //   function sendMessage() { 
-  //     socket.emit("send_msg", { message: message });
-  //   }
-
-  //   useEffect(() => {
-  //     socket.on("receive_msg", (data) => { 
-  //       setRecivedMessages(recivedMessages.concat(data.message));
-  //     });
-  //   }, [socket]);
-
-  // return (
-  //   <main className={styles.main}>
-
-  //     {
-  //       recivedMessages.map((msg, index) => {
-  //         return <div key={index}>{msg}</div>;
-  //       })
-  //     }
-  //     <div>
-
-  //       <input type="text" onChange={(e) => {
-  //         setMessage(e.target.value);
-  //       }} placeholder="Type a message" />
-
-  //       <button onClick={sendMessage} >Send</button>
-  //     </div>
-  //   </main>
-  // );
-
-
-
+ 
   return (
     <Box className={styles.fullHeight}>
 
